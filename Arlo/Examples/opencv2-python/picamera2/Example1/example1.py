bc4fd9103c426d4c8add2e42aa63944a47ed94c1
matias63 <fks561@alumni.ku.dk>
2023-09-21 13:48:17 +0200

    2

--- Arlo/Examples/opencv2-python/picamera2/Example1/example1.py
+++ Arlo/Examples/opencv2-python/picamera2/Example1/example1.py
@@ -114,9 +114,9 @@ while cv2.waitKey(4) == -1: # Wait for a key pressed event
     print(f"ArucoMarkerLength: {arucoMarkerLength}\n")
     aruco_corners, ids, rejectedImgPoints = cv2.aruco.detectMarkers(image, arucoDict)
     print(f"aruco_corners =\n {aruco_corners} \n\n ids =\n {ids} \n\n rejectImgPoints =\n {rejectedImgPoints}\n")
-    distortion_coeffs = None # we dont know the distortion 
+    # distortion_coeffs = None # we dont know the distortion 
     intrinsic_matrix = intrinsic()
-    rvecs, tvecs, objPoints = cv2.aruco.estimatePoseSingleMarkers(aruco_corners, arucoMarkerLength, intrinsic_matrix, np.array([]))
+    rvecs, tvecs, objPoints = cv2.aruco.estimatePoseSingleMarkers(aruco_corners, arucoMarkerLength, intrinsic_matrix, np.array([None]))
     print("tvecs: \n",tvecs)
     print("rvecs: \n",rvecs)
     print("objPoints:\n ",objPoints)
