ca3568e15362ffcc0c763a192a013c3d9ef06b47
matias63 <fks561@alumni.ku.dk>
2023-09-21 14:19:26 +0200

    2

--- Arlo/Examples/opencv2-python/picamera2/Example1/example1.py
+++ Arlo/Examples/opencv2-python/picamera2/Example1/example1.py
@@ -71,7 +71,7 @@ def intrinsic():
 
 
 def Beta(tvecs):
-    beta = np.arccos(np.dot((tvecs/math.sqrt(sum(i**2 for i in tvecs))),[0,0,1]))
+    beta = np.arccos(np.dot((tvecs/np.linalg.norm(tvecs))),[0,0,1])
     return beta
 
 def turn_angle(beta):
