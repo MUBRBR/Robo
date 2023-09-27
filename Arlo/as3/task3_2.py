d616fe6c127447aeda14b218af3512c78086f016
matias63 <fks561@alumni.ku.dk>
2023-09-27 14:19:04 +0200

    test

--- Arlo/as3/task3_2.py
+++ Arlo/as3/task3_2.py
@@ -245,7 +245,7 @@ while True:
     if (currDir != 1000):
         print("arlo should drive",currDist/100)
         print("arlo should angle",currDir)
-        arlo.RotateAngle(currDir)
+        arlo.RotateAngle(-currDir)
         arlo.DriveLength(currDist/100)
         break
     else:
