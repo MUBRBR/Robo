f30ffe4d373898c3336996d8101552336c289964
matias63 <fks561@alumni.ku.dk>
2023-11-01 13:56:16 +0100

    finish Task5.1

--- as5/Task5_1.py
+++ as5/Task5_1.py
@@ -261,7 +261,6 @@ def main():
                 print(f"angle: {-angle}")
                 roboarlo.RotateAngle(-angle)
 
-                roboarlo.Stop()
                 
                 
                 
@@ -274,15 +273,15 @@ def main():
                  
                 
                 # Multiplying Drive_dist by 100 because the field is in cm's
-                # if (distVecAsLength >= 0.99):
-                #     # roboarlo.RotateAngle(-angle)  # return back angle
-                #     particle_filter.move_particles(Drive_dist[0]*100/2 - est_pose[0], Drive_dist[1]*100/2 - est_pose[1], 0)
-                #     print(f"distVec/2: {distVecAsLength/2}")
-                #     roboarlo.DriveVector((Drive_dist[0]/2, Drive_dist[1]/2))
-                # else:
-                #     particle_filter.move_particles(Drive_dist[0]*100 - est_pose[0], Drive_dist[1]*100 - est_pose[1], 0)
-                #     print(f"distVec: {distVecAsLength}")
-                #     roboarlo.DriveVector(Drive_dist)
+                if (distVecAsLength >= 0.99):
+                    # roboarlo.RotateAngle(-angle)  # return back angle
+                    particle_filter.move_particles(Drive_dist[0]*100/2 - est_pose[0], Drive_dist[1]*100/2 - est_pose[1], 0)
+                    print(f"distVec/2: {distVecAsLength/2}")
+                    roboarlo.DriveVector((Drive_dist[0]/2, Drive_dist[1]/2))
+                else:
+                    particle_filter.move_particles(Drive_dist[0]*100 - est_pose[0], Drive_dist[1]*100 - est_pose[1], 0)
+                    print(f"distVec: {distVecAsLength}")
+                    roboarlo.DriveVector(Drive_dist)
                 
                
 
