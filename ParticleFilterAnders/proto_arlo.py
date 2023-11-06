0e3d05630798dcc93cf6746e9e5a366eb5277ab5
matias63 <fks561@alumni.ku.dk>
2023-11-06 11:28:56 +0100

    INIT_LOCALISE

--- ParticleFilterAnders/proto_arlo.py
+++ ParticleFilterAnders/proto_arlo.py
@@ -105,7 +105,7 @@ class proto_arlo():
         for objectID, dist, angle in zip(objectIDs, dists, angles):
             if (objectID == 1):
 
-                betterArlo.RotateAngle(-angle)
+                betterArlo.RotateAngle(np.degrees(-angle))
 
                 # print(f"{np.degrees(angle)}")
                 self.Log("Rotating towards target LM1 with degrees: ")
